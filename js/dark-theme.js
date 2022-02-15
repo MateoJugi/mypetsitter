286b8d3f08996f5cdb99a206dfab6357bf63eddc
Mateo <mateo.juginovic@gmail.com>
2022-02-15 12:11:40 +0100

    Added changes to dark-theme

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -5,19 +5,21 @@ if ( hours > 18 && hours < 24 || hours > 0 && hours < 5 ) {
 }
 
 function darktheme() {
-	$( '.dark-theme' ).toggleClass( 'dark-theme--active' );
+	$( '.dark-theme-trigger' ).toggleClass( 'dark-theme-trigger--active' );
 
-	if ( $( '.dark-theme' ).hasClass( 'dark-theme--active' ) ) {
+	$( 'body' ).toggleClass( 'dark-theme-change' );
+
+	if ( $( '.dark-theme-trigger' ).hasClass( 'dark-theme-trigger--active' ) ) {
 		$( '.dark-theme-wrapper' ).find( '.text' ).text( 'Light theme' );
 	} else {
 		$( '.dark-theme-wrapper' ).find( '.text' ).text( 'Dark theme' );
 	}
+}
 
-	$( '.js-background-color' ).toggleClass( 'background-color' );
-	$( '.js-surface-color' ).toggleClass( 'surface-color' );
-
-	$( '.js-on-surface-color' ).toggleClass( 'on-surface-color' );
-	$( '.js-surface-fill' ).toggleClass( 'surface-fill' );
+if ( $( '.dark-theme-trigger' ).hasClass( 'dark-theme-trigger--active' ) ) {
+	$( '.dark-theme-wrapper' ).find( '.text' ).text( 'Light theme' );
+} else {
+	$( '.dark-theme-wrapper' ).find( '.text' ).text( 'Dark theme' );
 }
 
 $( '.dark-theme-wrapper' ).click( darktheme );
